4242956279bbe29ca57341b0c213db3c0a7b14cf
Your Name <namita.dias06@gmail.com>
2023-03-15 11:05:35 +0000

    New app for product management. Added new bucket.

--- lib/product_management-stack.ts
+++ lib/product_management-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class ProductManagementStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productsBucket = new Bucket(this, 'product-managment-bucket');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductManagementQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'Product-Management-Bucket', {
+      value: productsBucket.bucketName,
+      exportName: 'Product-Management-Bucket'
+    })
   }
 }
