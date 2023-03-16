ede2b36909cb5f5e4932b5d3a756db556ee39930
Your Name <namita.dias06@gmail.com>
2023-03-16 10:29:59 +0000

    Deply product images to the S3 bucket.

--- lib/product_management-stack.ts
+++ lib/product_management-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
+import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
+import path from 'path';
 
 export class ProductManagementStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const productsBucket = new Bucket(this, 'product-managment-bucket');
+    const productsImagesBucket = new Bucket(this, 'product-image-bucket');
 
-    new cdk.CfnOutput(this, 'Product-Management-Bucket', {
-      value: productsBucket.bucketName,
-      exportName: 'Product-Management-Bucket'
+    new cdk.CfnOutput(this, 'Product-Image-Bucket', {
+      value: productsImagesBucket.bucketName,
+      exportName: 'Product-Image-Bucket'
     })
+
+    try {
+
+      new s3Deploy.BucketDeployment(this, 'deploy-images', {
+        sources: [
+          s3Deploy.Source.asset(path.join(__dirname, '..','images'))
+        ],
+        destinationBucket: productsImagesBucket
+      })
+      
+    } catch (err) {
+      console.log(`Error coping images to bucket ${productsImagesBucket}: ${err}`);
+    }
+
   }
 }
