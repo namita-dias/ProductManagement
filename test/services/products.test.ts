37694ad4ee4e444dacda45905d7e9c4a649e5bde
Your Name <namita.dias06@gmail.com>
2023-03-20 14:04:02 +0000

    Unit test to check if correct error message displayed

---
+++ test/services/products.test.ts
@@ -0,0 +1,16 @@
+import * as products from '../../services/products';
+
+
+describe('Testing the operations on products', () => {
+    test('sendFail - it should display route error when route not resolved', async() => {
+        
+        const event: any = {
+            resource: '/products/{Name}',
+            httpMethod: 'GET'
+        };
+
+        const result = await products.productHandler(event);
+        expect(result.statusCode).toBe(400);
+        expect(result.body).toContain('unsupported route');
+    })
+})
