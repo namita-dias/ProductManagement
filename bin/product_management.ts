7a4def97c6bba7cd738a877f412dab25b29287ea
Your Name <namita.dias06@gmail.com>
2023-04-03 12:05:59 +0100

    Removed renaming

--- bin/product_management.ts
+++ bin/product_management.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { ProductManagementStack } from '../lib/productManagementStack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { ProductManagementStack } from "../lib/product_management-stack";
 
 const app = new cdk.App();
-new ProductManagementStack(app, 'ProductManagementStack');
+new ProductManagementStack(app, "ProductManagementStack");
